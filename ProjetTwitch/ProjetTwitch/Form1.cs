0cead768786b28e4b8632b0c6def7c491cde8f8a
astus08 <astus08@hotmail.fr>
2016-12-31 22:39:33 +0100

    form

--- ProjetTwitch/ProjetTwitch/Form1.cs
+++ ProjetTwitch/ProjetTwitch/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projetTwitch;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace ProjetTwitch
 {
     public partial class Form1 : Form
     {
+        private twitchAPI twitchInit = new twitchAPI("oc651hjfox0rk94wahy25hpm8d17o5");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void validateButton_Click(object sender, EventArgs e)
+        {
+            string streamer = streamerBox.Text;
+
+            if (twitchInit.isOnline(streamer))
+            {
+                streamerState.Text = streamer + " is now ONLINE";
+                streamerState.ForeColor = Color.Green;
+            }
+            else
+            {
+                streamerState.Text = streamer + " is now OFFLINE";
+                streamerState.ForeColor = Color.Red;
+            }
         }
     }
 }
